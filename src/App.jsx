4990d36c7316d60cf9744936a5a04337a2dd6032
-api <elvisquilapi@gmail.com>
2020-07-12 01:59:25 -0400

    ajustes webpack

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ class App extends Component {
     render() {
         return (
             <div>
-                <h1>REACT!</h1>
+                <h1>Welcome to the Reactjs boilerplate!</h1>
             </div>
         )
     }
