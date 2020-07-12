3f7d79a1b1432c95467c8e3d4fcb6dc2fb89c32b
-api <elvisquilapi@gmail.com>
2020-07-12 02:39:07 -0400

    se agregar mas dependencias de los estilos

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
-import './App.css'
+import './App.scss'
 
 class App extends Component {
     render() {
         return (
-            <div>
-                <h1>Welcome to the Reactjs boilerplate!</h1>
+            <div className="container">
+                <img src="src/assets/logo.svg" alt="" className="logo" />
+                <h1>Welcome to the Reactjs boilerplate! 🚀</h1>
             </div>
         )
     }
