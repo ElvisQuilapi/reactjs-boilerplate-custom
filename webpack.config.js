52676a74c3ce83a1e9c61fa37f8d70d593f12791
-api <elvisquilapi@gmail.com>
2020-07-12 01:50:49 -0400

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = { 
+    entry: "./index.js",
+    output: {
+        path: path.join(__dirname, "/dist"),
+        filename: "build.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ["babel-loader"]
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    }, 
+    plugins: [ 
+        new HtmlWebpackPlugin({
+            template: "./src/index.html"
+        }),
+        new CleanWebpackPlugin()
+    ]
+};
